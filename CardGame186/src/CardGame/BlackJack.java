818a342711d144e2263058532978d4c36e1ed704
koshalshah <62960844+koshalshah@users.noreply.github.com>
2020-04-21 13:27:56 -0500

    Update BlackJack.java

--- CardGame186/src/CardGame/BlackJack.java
+++ CardGame186/src/CardGame/BlackJack.java
@@ -1,23 +1,24 @@
 package CardGame186;
-
+import java.util.Random;
 
 public class BlackJack {
 	public static void main(String[] args) {	
 	Player player = new Player();
 	Dealer dealer = new Dealer();
-	Deck newDeck = new Deck();
+	Random r = new Random(12345);
+	Deck newDeck = new Deck(52, r);
 	boolean playerTurn = true;
 	player.dealInitialCards(newDeck);
 	dealer.dealInitialCards(newDeck);
 	System.out.println(player.getPlayerValue());
 	System.out.println(dealer.getDealerValue());
 	 while (newDeck.returnDeckSize() > 0 && player.getPlayerValue() < 22 && dealer.getDealerValue() < 22 && dealer.getDealerValue() != 21) {
-		 if (playerTurn) {
+		 if (playerTurn && player.getPlayerValue() != 21) {
 		player.hit(newDeck);
 		playerTurn = false;
 		System.out.println(player.getPlayerValue());
 	 }
-		 else if (!playerTurn && dealer.getDealerValue() < 21){
+		 else if (!playerTurn && dealer.getDealerValue() != 21){
 		dealer.hit(newDeck);
 		playerTurn = true;
 		System.out.println(dealer.getDealerValue());
