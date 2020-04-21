84d328c5cdeb46eab1dd19c57eb9d626e134bfb0
koshalshah <62960844+koshalshah@users.noreply.github.com>
2020-04-21 13:53:32 -0500

    Update BlackJack.java

--- CardGame186/src/CardGame/BlackJack.java
+++ CardGame186/src/CardGame/BlackJack.java
@@ -5,22 +5,22 @@ public class BlackJack {
 	public static void main(String[] args) {	
 	Player player = new Player();
 	Dealer dealer = new Dealer();
-	Random r = new Random(123);
+	Random r = new Random(2332);
 	Deck newDeck = new Deck(52, r);
 	boolean playerTurn = true;
 	player.dealInitialCards(newDeck);
 	dealer.dealInitialCards(newDeck);
-	System.out.println(player.getPlayerValue());
-	System.out.println(dealer.getDealerValue());
+	System.out.println("Player: " + player.getPlayerValue());
+	System.out.println("Dealer: " + dealer.getDealerValue());
 	 while (newDeck.returnDeckSize() > 0 && player.getPlayerValue() < 22 && dealer.getDealerValue() < 22 && dealer.getDealerValue() != 21) {
 		 if (playerTurn && player.getPlayerValue() != 21) {
 		player.hit(newDeck);
 		playerTurn = false;
-		System.out.println(player.getPlayerValue());
+		System.out.println("Player: " + player.getPlayerValue());
 	 }
 		 else if (!playerTurn && dealer.getDealerValue() < 17){
 		dealer.hit(newDeck);
-		System.out.println(dealer.getDealerValue());
+		System.out.println("Dealer " + dealer.getDealerValue());
 		playerTurn = true;
 	 	}
 		 else if (player.getPlayerValue() < 21 && dealer.getDealerValue() >= 17) {
@@ -29,4 +29,3 @@ public class BlackJack {
 	  }
    }
 }
-
