5118cdacfde8a46150bde490b1035bbc16a5934d
codyp626 <62960554+codyp626@users.noreply.github.com>
2020-04-28 17:32:35 -0500

    src java files

---
+++ workingFiles/Dealer.java
@@ -0,0 +1,46 @@
+package CardGame;
+
+import java.util.ArrayList;
+
+public class Dealer {
+	private int dealerCards = 0;
+	private ArrayList<Card> hand = new ArrayList<Card>();
+	public Dealer() {
+	}
+	public static void main(String[] args) {
+	}
+	public int getDealerValue() {
+		return dealerCards;
+	}
+	public void drawCards(Deck newDeck) {
+		Card drawn = newDeck.drawCard();
+		if (drawn.returnRankValue() == 1 && dealerCards < 11) {
+			dealerCards += 11;
+		}
+		else if (drawn.returnRankValue() == 1 && dealerCards >= 11){
+			dealerCards += 1;
+		}
+		else if (drawn.returnRankValue() > 10) {
+			dealerCards += 10;
+		}
+		else {
+		dealerCards += drawn.returnRankValue();
+		}
+		hand.add(drawn);
+	}
+	public int ace(boolean oneOrEleven) {
+		if (oneOrEleven == true){
+			return 1;	
+		}
+		else {
+			return 11;
+		}
+	}
+	public void dealInitialCards(Deck newDeck) {
+		drawCards(newDeck);
+		drawCards(newDeck);
+	}
+	public void hit(Deck newDeck){
+		drawCards(newDeck);
+	}
+}
