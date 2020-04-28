91ca8574418e902074f0eea1f0f77e37da4355a4
koshalshah <62960844+koshalshah@users.noreply.github.com>
2020-04-28 12:39:53 -0500

    Update BlackJack.java

--- CardGame186/src/CardGame/BlackJack.java
+++ CardGame186/src/CardGame/BlackJack.java
@@ -2,17 +2,18 @@ package CardGame186;
 import java.util.Random;
 
 public class BlackJack {
-	public static void main(String[] args) {	
+	int playerBet = 0;
+	boolean playerTurn = true;
+	public BlackJack() {	
 	Player player = new Player();
 	Dealer dealer = new Dealer();
 	Random r = new Random(2332);
 	Deck newDeck = new Deck(52, r);
-	boolean playerTurn = true;
 	player.dealInitialCards(newDeck);
 	dealer.dealInitialCards(newDeck);
 	System.out.println("Player: " + player.getPlayerValue());
 	System.out.println("Dealer: " + dealer.getDealerValue());
-	 while (newDeck.returnDeckSize() > 0 && player.getPlayerValue() < 22 && dealer.getDealerValue() < 22 && dealer.getDealerValue() != 21) {
+	while (newDeck.returnDeckSize() > 0 && player.getPlayerValue() < 22 && dealer.getDealerValue() < 22 && dealer.getDealerValue() != 21) {
 		 if (playerTurn && player.getPlayerValue() != 21) {
 		player.hit(newDeck);
 		playerTurn = false;
@@ -25,7 +26,17 @@ public class BlackJack {
 	 	}
 		 else if (player.getPlayerValue() < 21 && dealer.getDealerValue() >= 17) {
 			 playerTurn = true;
+		 }
 		}
-	  }
-   }
+	}
+	public void bet(int bet){
+		playerBet = bet;
+	}
+	public void playerHit() {
+		playerTurn = true;
+	}
+	public void playerStand() {
+		playerTurn = false;
+	}
 }
+
