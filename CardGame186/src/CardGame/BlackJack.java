1f4c499fa5f0ecebaa6b731b4caa09895af570e2
koshalshah <62960844+koshalshah@users.noreply.github.com>
2020-04-28 12:57:26 -0500

    Update BlackJack.java

--- CardGame186/src/CardGame/BlackJack.java
+++ CardGame186/src/CardGame/BlackJack.java
@@ -3,40 +3,48 @@ import java.util.Random;
 
 public class BlackJack {
 	int playerBet = 0;
+	int playerWinnings = 0;
 	boolean playerTurn = true;
-	public BlackJack() {	
+	boolean dealerWin = false;
 	Player player = new Player();
 	Dealer dealer = new Dealer();
 	Random r = new Random(2332);
 	Deck newDeck = new Deck(52, r);
+	
+	
+	public BlackJack() {	
 	player.dealInitialCards(newDeck);
 	dealer.dealInitialCards(newDeck);
-	System.out.println("Player: " + player.getPlayerValue());
-	System.out.println("Dealer: " + dealer.getDealerValue());
-	while (newDeck.returnDeckSize() > 0 && player.getPlayerValue() < 22 && dealer.getDealerValue() < 22 && dealer.getDealerValue() != 21) {
-		 if (playerTurn && player.getPlayerValue() != 21) {
-		player.hit(newDeck);
-		playerTurn = false;
-		System.out.println("Player: " + player.getPlayerValue());
-	 }
-		 else if (!playerTurn && dealer.getDealerValue() < 17){
+	
+	while (newDeck.returnDeckSize() > 0 && dealer.getDealerValue() < 21) {
+		if (!playerTurn && dealer.getDealerValue() < 17){
 		dealer.hit(newDeck);
-		System.out.println("Dealer " + dealer.getDealerValue());
-		playerTurn = true;
 	 	}
-		 else if (player.getPlayerValue() < 21 && dealer.getDealerValue() >= 17) {
-			 playerTurn = true;
+		 else if (player.getPlayerValue() < 21 && dealer.getDealerValue() > player.getPlayerValue()) {
+			 dealerWin = true;
+		 }
+		 else if (player.getPlayerValue() <= 21 && dealer.getDealerValue() < player.getPlayerValue()) {
+			 dealerWin = false;
+			 playerWinnings = playerBet*2;
+		 }
+		 else if (player.getPlayerValue() == dealer.getDealerValue()) {
+			 playerWinnings = playerBet;
 		 }
 		}
 	}
+	
 	public void bet(int bet){
 		playerBet = bet;
 	}
 	public void playerHit() {
-		playerTurn = true;
+		 if (playerTurn && player.getPlayerValue() < 21) {
+				player.hit(newDeck);
+			 }
+		 else {
+			 playerTurn = false;
+		 }
 	}
 	public void playerStand() {
 		playerTurn = false;
 	}
 }
-
